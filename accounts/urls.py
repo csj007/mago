8d74870f37a4cb98dbbf4796cf4e1c16cbf24937
AtlasZ <651570477@qq.com>
2025-07-23 13:23:50 +0800

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,13 +3,14 @@ from . import views
 from django.views.generic.base import RedirectView
 
 urlpatterns = [
-    path('', RedirectView.as_view(url='/login/')),  # 自动跳转到 /login
+    path('', RedirectView.as_view(url='/login/')),
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
     path('dashboard/', views.medicine_list, name='medicine_list'),
     path('add/', views.add_medicine, name='add_medicine'),
     path('edit/<int:medicine_id>/', views.edit_medicine, name='edit_medicine'),
     path('delete/<int:medicine_id>/', views.delete_medicine, name='delete_medicine'),
+    path('export/', views.export_medicine, name='export_medicine'),
     path('home/', views.home, name='home'),
     path('api/get-medicine-codes/', views.get_medicine_codes, name='get_medicine_codes'),
 ]
