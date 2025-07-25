94a4b6553abe1a24d5c33d9b5aae34a526e9d7df
AtlasZ <651570477@qq.com>
2025-07-25 16:10:26 +0800

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -15,4 +15,10 @@ urlpatterns = [
     path('api/get-medicine-codes/', views.get_medicine_codes, name='get_medicine_codes'),
     path('api/find_medicine_codes/', views.find_medicine_codes, name='find_medicine_codes'),
     path('register/', views.register, name='register'),
+    path('api/save_recipe/', views.save_recipe, name='save_recipe'),
+    path('api/load_recipe/<int:recipe_id>/', views.load_recipe, name='load_recipe'),
+    path('api/list_recipes/', views.list_recipes, name='list_recipes'),
+    path('api/search_recipes/', views.search_recipes, name='search_recipes'),
+    path('api/delete_recipe/<int:recipe_id>/', views.delete_recipe, name='delete_recipe'),
+    path('api/rename_recipe/<int:recipe_id>/', views.rename_recipe, name='rename_recipe'),
 ]
